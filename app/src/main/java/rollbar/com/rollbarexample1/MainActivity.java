391416cb680e6d09079f8981682ad6f6077f0c6e
Wahid Ali <wahid.ali@psquickit.com>
2018-04-19 19:52:42 +0530

    Added android repo

---
+++ app/src/main/java/rollbar/com/rollbarexample1/MainActivity.java
@@ -0,0 +1,31 @@
+package rollbar.com.rollbarexample1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.rollbar.android.Rollbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Rollbar rollbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.clickMe);
+        Rollbar.init(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (rollbar == null) {
+                    Log.d("yo", "Rollbar is null, initialize before using");
+                    return;
+                }
+                String test=null;
+                test.toString();
+            }
+        });
+    }
+}
